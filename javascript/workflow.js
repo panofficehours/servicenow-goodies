db65d28271c906e5c90109e8d97baabcb32b6e82
Network Automation <panofficehours@proton.me>
2023-01-27 13:13:01 -0800

    filling out security policy parameters

--- javascript/workflow.js
+++ javascript/workflow.js
@@ -1,8 +1,16 @@
 try {
     var r = new sn_ws.RESTMessageV2("Ansible Test", "Default GET");
     // SITE SECTION
-    r.setStringParameter("src_zone", current.variables.src_zone.toString());
-    r.setStringParameter("name", current.variables.name.toString());
+    r.setStringParameter("device_group", current.variables.device_group.toString());
+    r.setStringParameter("rule_name", current.variables.rule_name.toString());
+    r.setStringParameter("description", current.variables.description.toString());
+    r.setStringParameter("tag_name", current.variables.tag_name.toString());
+    r.setStringParameter("source_zone", current.variables.source_zone.toString());
+    r.setStringParameter("source_ip", current.variables.source_ip.toString());
+    r.setStringParameter("destination_zone", current.variables.destination_zone.toString());
+    r.setStringParameter("destination_ip", current.variables.destination_ip.toString());
+    r.setStringParameter("application", current.variables.application.toString());
+    r.setStringParameter("action", current.variables.action.toString());
     var response = r.execute();
     var responseBody = response.getBody();
     var httpStatus = response.getStatusCode();
